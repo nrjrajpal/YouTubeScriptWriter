3064496b1d70ec344f730a293b68fffecb5414fd
notnrj <nrjrpl007@gmail.com>
2024-12-17 18:18:13 +0530

    remove setters from SyntheticAgent class

--- backend/APIs/__init__.py
+++ backend/APIs/__init__.py
@@ -1,7 +1,7 @@
-from .temp_api import temp_blueprint
-# from backend.APIs.agent_api import agent_blueprint
+from APIs.temp_api import temp_blueprint, sign_up_2_blueprint
 
 def api_blueprints(app):
     # Register blueprints
     app.register_blueprint(temp_blueprint)
+    app.register_blueprint(sign_up_2_blueprint)
     # app.register_blueprint(agent_blueprint, url_prefix="/agent")
