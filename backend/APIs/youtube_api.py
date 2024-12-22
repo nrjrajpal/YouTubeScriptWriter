ce51576d7f0ce3040467809a0c8bc8450b92a336
Ishan Vyavahare <ishanvyavahare@Ishans-MacBook-Air.local>
2024-12-22 13:52:59 +0530

    add get and set apis and functions for youtube

--- backend/APIs/youtube_api.py
+++ backend/APIs/youtube_api.py
@@ -59,6 +59,62 @@ def fetchVideoTranscript():
     except Exception as e:
         return jsonify({"error": "An error occurred: " + e.message or e, "success": False}), 500
 
+@youtube_blueprint.route('/setVideoIDs', methods=['POST'])
+def setVideoIDs():
+    try:
+        data = request.get_json()
+        userEmail = data.get('userEmail')
+        projectID = data.get('projectID')
+        video_urls = data.get('video_urls')
+
+        if not userEmail:
+            return jsonify({"error": "Missing required field: userEmail", "success": False}), 400
+
+        if not projectID:
+            return jsonify({"error": "Missing required field: projectID", "success": False}), 400
+        
+        if not video_urls:
+            return jsonify({"error": "Missing required field: video_urls", "success": False}), 400
+        
+        # truncating urls into ids 
+        video_ids = []
+        for video_url in video_urls:
+            video_ids.append(video_url.split("=")[-1])
+            
+        ytagent = YouTubeAgent(projectID, userEmail)
+        message = ytagent.setVideoIDs(video_ids)
+
+        return jsonify({"message":message, "success": True}), 200
+    
+    except (KeyNotFoundError,ProjectNotFoundError) as e:
+        return jsonify({"error": e.message, "success": False}), 404
+    except Exception as e:
+        return jsonify({"error": "An error occurred: " + e.message or e, "success": False}), 500
+    
+
+@youtube_blueprint.route('/getVideoIDs', methods=['POST'])
+def getVideoIDs():
+    try:
+        data = request.get_json()
+        userEmail = data.get('userEmail')
+        projectID = data.get('projectID')
+
+        if not userEmail:
+            return jsonify({"error": "Missing required field: userEmail", "success": False}), 400
+
+        if not projectID:
+            return jsonify({"error": "Missing required field: projectID", "success": False}), 400
+                    
+        ytagent = YouTubeAgent(projectID, userEmail)
+        videoIDs = ytagent.getVideoIDs()
+
+        return jsonify({"message":"Successfully retrieved sourced youtube video ids", "videoIDs": videoIDs, "success": True}), 200
+    
+    except (KeyNotFoundError,ProjectNotFoundError) as e:
+        return jsonify({"error": e.message, "success": False}), 404
+    except Exception as e:
+        return jsonify({"error": "An error occurred: " + e.message or e, "success": False}), 500
+
 
 @youtube_blueprint.route('/fetchVideoMetadata', methods=['POST'])
 def fetchVideoMetadata():
