2ba913b5f7e1738390c3bf7efe7182592cc9b3b0
Ishan <ishanvyavahare@gmail.com>
2024-12-29 19:28:32 +0530

    fix minor ui changes

--- frontend/src/app/project/layout.tsx
+++ frontend/src/app/project/layout.tsx
@@ -6,9 +6,10 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         <div className="min-h-screen flex flex-col">
             <header className="p-4 border-b">
                 <Link href="/dashboard" passHref>
-                    <Button variant="outline" className="text-md border-[1px] border-white">
+                <Button className="font-script border border-gray-600 text-sm h-10 w-auto px-6 rounded-xl bg-gray-900 text-white hover:bg-gray-800 font-medium">Dashboard</Button>
+                    {/* <Button variant="outline" className="text-md border-[1px] border-white">
                         Dashboard
-                    </Button>
+                    </Button> */}
                 </Link>
             </header>
             <main className="flex-grow">
