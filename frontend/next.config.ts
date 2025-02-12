0ac20e3dfab5f460302e034e3f17ee7df31573ac
Adi-Samudra <adityasamudra369@gmail.com>
2025-02-12 12:44:42 +0530

    cloudflare config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
@@ -7,5 +8,8 @@ const nextConfig: NextConfig = {
   },
   /* config options here */
 };
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform();
+}
 
 export default nextConfig;
